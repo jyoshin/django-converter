17512e901597417b19d7bf12de76e0088db09b6a
jyoshin <gleb.parkdes@gmail.com>
2023-10-18 00:46:38 +0600

    Changes

--- base/urls.py
+++ base/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.basicResponse, name='tasks'),
+    path('', views.basic_response, name='converter'),
 ]
--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.decorators.http import require_http_methods
 
-def basicResponse(request):
+@require_http_methods(['GET'])
+def basic_response(request):
     return HttpResponse('Converter')
